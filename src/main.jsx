c61951b4b22f574fcf74bbe837b044f616092529
lcht2701 <lecanhhoangthanh@gmail.com>
2024-05-11 22:06:34 +0700

    mod: custom Manager Layout from template

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import {
+  Route,
+  Routes,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import { Login } from "./pages/auth/Login";
 import { HomeManager } from "./pages/manager/Home";
 import NotFound from "./pages/error/NotFound";
+import ManagerLayout from "./layouts/ManagerLayout";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/manager",
-    element: <HomeManager />,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Login />} />
+      <Route path="manager" element={<ManagerLayout />}>
+        <Route index element={<HomeManager />} />
+        {/* Here you can add more nested routes inside the manager path */}
+        {/* <Route path="dashboard" element={<Dashboard />} />
+         <Route path="tickets" element={<Tickets />} /> */}
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
